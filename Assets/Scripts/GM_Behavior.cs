5bbbc98e7f7d2630fb875801bd7e02fae7f8bdbf
Unknown <jayj593@gmail.com>
2019-01-25 21:47:09 -0800

    Added my tiny bit of work
    Enjoy

---
+++ Assets/Scripts/GM_Behavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_Behavior : MonoBehaviour
+{
+    public Camera Cam;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+            RaycastHit hit ;
+            Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f ))
+            {
+                print(hit.collider);
+                Debug.DrawLine(ray.origin, ray.GetPoint(100), Color.blue);
+
+            }
+    }
+}
---
+++ Assets/Scripts/InteractablesBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablesBase : MonoBehaviour
+{
+    private bool isActivated = false;
+    private bool minigame;
+
+    private GameObject GM;
+
+    public Collider myCollider;
+
+    public AudioSource mySound;
+
+    public virtual void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        mySound = GetComponent<AudioSource>();
+        GM = GameObject.Find("GM");
+    }
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Interact();
+        }
+    }
+
+    public virtual void OnSelected()
+    {
+        isActivated = true;
+    }
+
+    public virtual void Deselect()
+    {
+        isActivated = false;
+    }
+
+    public virtual void Interact()
+    {
+            // Play animation if exists
+            // Play Sound if exists
+            // Tell GM to begin minigame
+    }
+}
