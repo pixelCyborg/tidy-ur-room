a8cb98c77171d4ebbfb82999d61d5653390e4d91
Unknown <jayj593@gmail.com>
2019-01-26 11:20:20 -0800

    Saturday Morning
    Asset folder organization
    Mouse tracking
    Dishes minigame (incomplete)

---
+++ Assets/Scripts/MinigameBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameBase : MonoBehaviour
+{
+    private GameObject[] dirt;
+    public virtual void OnAwake()
+    {
+        dirt = GameObject.FindGameObjectsWithTag("dirt");
+    }
+
+    public virtual void Update()
+    {
+        
+    }
+
+    public virtual void Clean()
+    {
+
+    }
+}
