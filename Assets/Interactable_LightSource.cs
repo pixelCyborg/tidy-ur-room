43dc987a2d40d05a46ba484122dbbfe28c313160
Harrison Cote <hcote@d2.com>
2019-01-26 16:45:50 -0800

    Light stuff

---
+++ Assets/Interactable_LightSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_LightSource : InteractablesBase
+{
+    Light[] lights;
+
+    void Start() {
+        lights = GetComponentsInChildren<Light>();
+        for (int i = 0; i < lights.Length; i++) {
+            lights[i].enabled = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    public override void Interact()
+    {
+        base.Interact();
+        for (int i = 0; i < lights.Length; i++) {
+            lights[i].enabled = true;
+        }
+    }
+}
