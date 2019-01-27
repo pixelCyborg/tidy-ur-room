14fbb1c7525dd6ac658cbee5ba2bd04efdefb20f
Harrison Cote <hcote@d2.com>
2019-01-27 14:21:57 -0800

    Fixing some stuuuufff

---
+++ Assets/CozyMeter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CozyMeter : MonoBehaviour
+{
+    public CozyMeter instance;
+    public RectTransform cozinessMeter; 
+    float origWidth;
+    float currentCoziness;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCoziness = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 sizeDelta = cozinessMeter.sizeDelta;;
+        float currentWidth = origWidth * currentCoziness;
+        sizeDelta.x = currentWidth;
+
+        cozinessMeter.sizeDelta = Vector2.Lerp(cozinessMeter.sizeDelta, sizeDelta, Time.deltaTime * 3.0f);
+    }
+}
